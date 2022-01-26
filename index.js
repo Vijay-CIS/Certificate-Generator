69bc424fe8d28c78dbc877d94e999bb3428eb4c8
Vijay-CIS <vijay.annamalai001@gmail.com>
2022-01-26 22:46:31 +0530

    Everything fine

---
+++ index.js
@@ -0,0 +1,92 @@
+const userName = document.getElementById("name");
+const submitBtn = document.getElementById("submitBtn");
+const d = document.getElementById("date");
+// const d = document.getElementById("date");
+
+const { PDFDocument, rgb, degrees } = PDFLib;
+
+// To create a Current date
+let today = new Date()
+let month = today.getMonth()+1;
+let year = today.getFullYear();
+let date = today.getDate();
+
+let current_date = `${date}/${month}/${year}`;
+// document.write(current_date);
+
+
+const capitalize = (str, lower = false) =>
+  (lower ? str.toLowerCase() : str).replace(/(?:^|\s|["'([{])+\S/g, (match) =>
+    match.toUpperCase()
+  );
+
+submitBtn.addEventListener("click", () => {
+  const val = capitalize(userName.value);
+  current_date=d.value;
+
+  //check if the text is empty or not
+  if (val.trim() !== "" && userName.checkValidity()) {
+    // console.log(val);
+    generatePDF(val);
+  } else {
+    userName.reportValidity();
+  }
+});
+
+const generatePDF = async (name,) => {
+  const existingPdfBytes = await fetch("./cert.pdf").then((res) =>
+    res.arrayBuffer()
+  );
+
+  // Load a PDFDocument from the existing PDF bytes
+  const pdfDoc = await PDFDocument.load(existingPdfBytes);
+  pdfDoc.registerFontkit(fontkit);
+
+  //get font
+  const fontBytes = await fetch("./Sanchez-Regular.ttf").then((res) =>
+    res.arrayBuffer()
+  );
+
+  // Embed our custom font in the document
+  const SanChezFont = await pdfDoc.embedFont(fontBytes);
+
+  // Get the first page of the document
+  const pages = pdfDoc.getPages();
+  const firstPage = pages[0];
+
+  // Draw a string of text in the certificate
+  firstPage.drawText(name, {
+    x: 300,
+    y: 350,
+    size: 58,
+    font: SanChezFont,
+    color: rgb(0.2, 0.84, 0.67),
+  });
+
+
+  firstPage.drawText(current_date, {
+    x: 600,
+    y:  78,
+    size: 15,
+    font: SanChezFont,
+    color:rgb(0,0,0),
+  });
+
+
+  // Serialize the PDFDocument to bytes (a Uint8Array)
+  const pdfBytes = await pdfDoc.save();
+  console.log("Done creating");
+
+
+  var file = new File(
+    [pdfBytes],
+    "Your Certificate.pdf",
+    {
+      type: "application/pdf;charset=utf-8",
+    }
+  );
+ saveAs(file);
+};
+
+// init();
+
